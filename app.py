4884fc1f21f4feb3e7ec8458839d37e5537fa861
csantabearing <camilo@bearing.ai>
2022-08-12 12:33:30 -0500

    Cleanup

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from fastapi import FastAPI, File, UploadFile
 from emotions import Sentiment
 from mangum import Mangum
 import numpy as np
-import base64
 import cv2
 import io
 
@@ -17,15 +16,12 @@ async def sentiment(file: UploadFile = File(...)):
     contents=await file.read()
     nparr = np.fromstring(contents, np.uint8)
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    img_dimensions = str(img.shape)
     return_img = model.predict(img)
     _, png_img = cv2.imencode('.PNG', return_img)
-    encoded_img = base64.b64encode(png_img)
     return StreamingResponse(io.BytesIO(png_img.tobytes()), media_type="image/png")
 
 @app.get("/", tags=["Health Check"])
-def root():
+async def root():
     return {"message": "Ok"}
-
 
 handler = Mangum(app=app)
