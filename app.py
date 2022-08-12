a4a4732a258073d8b7661862ebc15e8bad6fbfc3
csantabearing <camilo@bearing.ai>
2022-08-11 21:51:26 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,36 @@
+from starlette.responses import StreamingResponse
+from fastapi import FastAPI, File, UploadFile
+from emotions import Sentiment
+from mangum import Mangum
+import numpy as np
+import base64
+import cv2
+import io
+
+model_path = './model.h5'
+model=Sentiment(model_path)
+
+app = FastAPI(title='Serverless Lambda FastAPI') #root_path="/Prod/"
+
+@app.get("/sentiment", tags=["Sentiment Analysis"])
+async def sentiment(file: UploadFile = File(...)):
+    contents=await file.read()
+    nparr = np.fromstring(contents, np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    img_dimensions = str(img.shape)
+    return_img = model.predict(img)
+    _, png_img = cv2.imencode('.PNG', return_img)
+    encoded_img = base64.b64encode(png_img)
+    return StreamingResponse(io.BytesIO(png_img.tobytes()), media_type="image/png")
+    return {
+        'filename': file.filename,
+        'dimensions': img_dimensions,
+        'encoded_img': encoded_img,
+    }
+
+@app.get("/", tags=["Health Check"])
+def root():
+    return {"message": "Ok"}
+
+
+handler = Mangum(app=app)
---
+++ emotions.py
@@ -0,0 +1,45 @@
+from tensorflow.keras.layers import Dense, Dropout, Flatten
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D
+import numpy as np
+import cv2
+
+class Sentiment():
+    def __init__(self,model_path):
+        model = Sequential()
+
+        model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48,48,1)))
+        model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+
+        model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+
+        model.add(Flatten())
+        model.add(Dense(1024, activation='relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(7, activation='softmax'))
+        
+        model.load_weights('model.h5')
+        
+        self.model=model
+        self.emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
+        self.facecasc = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+    def predict(self,frame):
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = self.facecasc.detectMultiScale(gray,scaleFactor=1.3, minNeighbors=5)
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (255, 0, 0), 2)
+            roi_gray = gray[y:y + h, x:x + w]
+            cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
+            prediction = self.model.predict(cropped_img)
+            maxindex = int(np.argmax(prediction))
+            cv2.putText(frame, self.emotion_dict[maxindex], (x+20, y-60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+        return frame
